5c58c88b62fb5d195229d346d01cc7f9ccd20cf3
maqss <maksymgulko@gmail.com>
2024-08-24 20:35:14 +0200

    maybe final result

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,13 +3,18 @@ export default function handleSubmit(event, renderImages) {
   const key = '45559560-5df08bb83c1629c82dd907879';
   const form = event.target;
   const searchResult = form.elements.picture.value.trim();
+  const loader = document.querySelector('.loader');
   const url =
     'https://pixabay.com/api/?key=' +
     key +
     '&q=' +
     encodeURIComponent(searchResult) +
-    '&per_page=45';
+    '&per_page=45' +
+    '&image_type=photo' +
+    '&orientation=horizontal' +
+    '&safesearch=true';
 
+  loader.style.display = 'inline-block';
   fetch(url)
     .then(response => {
       if (!response.ok) {
@@ -20,5 +25,8 @@ export default function handleSubmit(event, renderImages) {
     .then(images => {
       renderImages(images);
     })
-    .catch(error => console.log('Error:', error));
+    .catch(error => console.log('Error:', error))
+    .finally(() => {
+      loader.style.display = 'none';
+    });
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,9 +1,15 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 export default function renderImages(images) {
   const gallery = document.querySelector('.image-gallery');
   gallery.innerHTML = '';
+  const lightbox = new SimpleLightbox('.gallery-link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
 
   if (images.hits.length > 0) {
     const markup = images.hits
@@ -26,17 +32,27 @@ export default function renderImages(images) {
                 alt="${tags}"
               />
             </a>
+            <div class="overlay">
+            <div class="info">
+                <div class="overlay-item">Likes<br>${likes}</div>
+                <div class="overlay-item">Views<br>${views}</div>
+                <div class="overlay-item">Comments<br>${comments}</div>
+                <div class="overlay-item">Downloads<br>${downloads}</div>
+                </div>
+            </div>
           </li>`;
       })
       .join('');
 
     gallery.innerHTML = markup;
 
-    new SimpleLightbox('.gallery-link', {
-      captionsData: 'alt',
-      captionDelay: 250,
-    });
+    lightbox.refresh();
   } else {
-    gallery.innerHTML = '<p>No images found.</p>';
+    iziToast.show({
+      message: `❌ Sorry, there are no images matching your search query.`,
+      position: 'topRight',
+      backgroundColor: '#F44336',
+      messageColor: '#fff',
+    });
   }
 }
